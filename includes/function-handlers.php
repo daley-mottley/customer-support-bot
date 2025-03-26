ba957041bc722eca39055dea8bb24f426c369b6a
Daley Mottley <daley.mottley@hotmail.com>
2025-03-26 22:07:18 +0430

    feat: update error handling

--- includes/function-handlers.php
+++ includes/function-handlers.php
@@ -4,42 +4,48 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-// Handle function calls from the assistant
+/**
+ * Handle function calls from the assistant.
+ *
+ * @param string $function_name The name of the function to call.
+ * @param array  $arguments     The arguments for the function.
+ * @return array The function response or an error array.
+ */
 function vacw_handle_function_call($function_name, $arguments) {
     switch ($function_name) {
         case 'check_availability':
-            // Send a POST request to the check_availability webhook
             $response = wp_remote_post(
                 'https://hook.us1.make.com/qqsdn32ixz7xdgfrn1x6ld1ss052xmsu',
-                array(
-                    'headers' => array('Content-Type' => 'application/json'),
-                    'body'    => json_encode($arguments),
-                )
+                [
+                    'headers' => ['Content-Type' => 'application/json'],
+                    'body' => json_encode($arguments),
+                ]
             );
             if (is_wp_error($response)) {
-                return array('error' => 'Unable to check availability.');
+                vacw_log('Error checking availability: ' . $response->get_error_message(), 'error');
+                return ['error' => 'Unable to check availability at this time.'];
             } else {
                 return json_decode(wp_remote_retrieve_body($response), true);
             }
             break;
 
         case 'book_appointment':
-            // Send a POST request to the book_appointment webhook
             $response = wp_remote_post(
                 'https://hook.us1.make.com/no9a7fqgfwp9csvaw7gh2gc5lqroswjd',
-                array(
-                    'headers' => array('Content-Type' => 'application/json'),
-                    'body'    => json_encode($arguments),
-                )
+                [
+                    'headers' => ['Content-Type' => 'application/json'],
+                    'body' => json_encode($arguments),
+                ]
             );
             if (is_wp_error($response)) {
-                return array('error' => 'Unable to book appointment.');
+                vacw_log('Error booking appointment: ' . $response->get_error_message(), 'error');
+                return ['error' => 'Unable to book appointment at this time.'];
             } else {
                 return json_decode(wp_remote_retrieve_body($response), true);
             }
             break;
 
         default:
-            return array('error' => 'Function not found.');
+            return ['error' => 'Function not found.'];
     }
 }
